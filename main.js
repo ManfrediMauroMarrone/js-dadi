7a3127230fdcc7ea130642fc2cc0e3cb498c4fbd
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-10-14 22:49:26 +0200

    dadi corretto

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ var dado = [1, 2, 3, 4, 5, 6];
 var lanci = [];
 
 for (var i = 0; i <= 1; i++) {
-    var valoreLancio = Math.floor(Math.random() * dado.lenght);
+    var valoreLancio = Math.floor(Math.random() * dado.length);
     var giocata = dado[valoreLancio];
-    lanci.push(giocata)
+    lanci.push(giocata);
 }
 
-if (lanci[1] > lanci[2]){
+if (lanci[0] > lanci[1]){
     console.log('Vince il primo giocatore' + lanci);
 
-} else if (lanci[1] < lanci[2]) {
+} else if (lanci[0] < lanci[1]) {
     console.log('Vince il secondo giocatore' + lanci);
 } else {
     console.log('Parità' + lanci);
